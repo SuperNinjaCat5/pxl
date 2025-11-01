f4ee05bb71397721653b1a112ff061ea379ca825
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-11-01 16:43:32 -0400

    Enhance button styling and animation for Bitvault entry; update button text and class for consistency

--- website/src/routes/home/+page.svelte
+++ website/src/routes/home/+page.svelte
@@ -28,7 +28,7 @@
 <div>
   <h1>Welcome to pxl</h1>
   {#if $page.data.session}<h5>Signed in as {$page.data.session.user?.email}</h5>{:else}<h5>Not signed in</h5>{/if}
-  <a href="/bitvualt"><button>Edit the canvas</button></a>
+  <a href="/bitvualt"><button class="bitvualt-button" data-text="Enter the Bitvault">Enter the Bitvault</button></a>
   
   <button on:click={() => signOut()}>Sign out</button>
 </div>
