fd462190cb82270187c8023434e105353559eadf
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-11-01 14:45:45 -0400

    Refactor layout structure and styles for home page; enhance responsiveness and visual consistency

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -3,15 +3,15 @@
 </svelte:head>
 <script>import pxlTitle from '$lib/assets/pxl-title.png';</script>
 
-<div>
+<div class="home-wrapper">
+  <div class="hometext-center">
+    <p class="welcome">
+      Welcome to
+      <img src={pxlTitle} alt="PXL logo" class="pxl-logo" />
+    </p>
+    <br><br>
+    <a href=/login><button>Sign In?</button></a>
 
-  <p class="welcome text-center">
-    Welcome to
-    <img src={pxlTitle} alt="PXL logo" class="pxl-logo" />
-  </p>
-
-  <a href=/login><button>Sign In?</button></a>
-
-  <a href=/home><p>rlly secret bypass</p></a>
-
+    <a href=/home><p style="font-size: 5px; margin: 15px;">rlly secret bypass</p></a>
+  </div>
 </div>
