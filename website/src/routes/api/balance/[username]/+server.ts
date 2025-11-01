f1d60f296e1190b4decffc19af6398b1be5e2e68
Nico Smith <web@niiccoo2.xyz>
2025-11-01 13:55:18 -0400

    Remove vibey codey

--- website/src/routes/api/balance/[username]/+server.ts
+++
@@ -1,8 +0,0 @@
-import { balances } from '$lib/balances';
-import { json } from '@sveltejs/kit';
-
-export async function GET({ params }) {
-  const username = params.username;
-  const balance = balances.get(username) || 0;
-  return json({ balance });
-}
--- website/src/routes/api/balance/increase/+server.ts
+++
@@ -1,9 +0,0 @@
-import { balances } from '$lib/balances';
-import { json } from '@sveltejs/kit';
-
-export async function POST({ request }) {
-  const { username, amount } = await request.json();
-  const current = balances.get(username) || 0;
-  balances.set(username, current + amount);
-  return json({ success: true });
-}
