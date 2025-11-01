a652739322d0ff980e5b3cfb3624f5ad9db04051
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-11-01 15:37:46 -0400

    Add nodemailer dependency for email handling

--- website/src/routes/home/+page.svelte
+++ website/src/routes/home/+page.svelte
@@ -3,25 +3,31 @@
 </svelte:head>
 
 <script lang="ts">
-  import { signIn, signOut } from "@auth/sveltekit/client"
-  import { page } from "$app/stores"
-  import { goto } from '$app/navigation'
+  import { signIn, signOut } from "@auth/sveltekit/client";
+  import { page } from "$app/stores";
+  import { goto } from '$app/navigation';
   import { onMount } from "svelte";
 
+  const allowed_emails = ["ben.elliott.2021@gmail.com", "web@niiccoo2.xyz"];
+  let email: string | null = null
+
   onMount(() => {
-    if ($page.data.session) {
-      return;
+    email = $page.data.session?.user?.email ?? null; 
+
+    if (!email || !allowed_emails.includes(email)) {
+      goto('/');
     }
-    else {
-      goto('/')
-    }
-  })
+  });
 </script>
 
+{#if email && allowed_emails.includes(email)}
 <div>
   <h1>Welcome to pxl</h1>
   {#if $page.data.session}<h5>Signed in as {$page.data.session.user?.email}</h5>{:else}<h5>Not signed in</h5>{/if}
-  <a href="/bitvualt"><button>Edit the canvas?</button></a>
+  <a href="/bitvualt"><button>Edit the canvas</button></a>
   
   <button on:click={() => signOut()}>Sign out</button>
 </div>
+{:else}
+<p>Loser</p>
+{/if}
