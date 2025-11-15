53ff54bfd915c86635f45ca8a8e4edc273a7e9c4
Nico Smith <web@niiccoo2.xyz>
2025-11-15 10:07:51 -0500

    Change /pixels/get to by defualt return whole canvas

--- src/routes/api/pixels/get/+server.ts
+++ src/routes/api/pixels/get/+server.ts
@@ -5,10 +5,10 @@ export const GET: RequestHandler = async ({ url }) => {
     console.log('got request for pixels');
     const qp = url.searchParams;
 
-    const x0 = qp.has('x0') ? Number.parseInt(qp.get('x0')!, 10) : undefined;
-    const x1 = qp.has('x1') ? Number.parseInt(qp.get('x1')!, 10) : undefined;
-    const y0 = qp.get('y0') ? Number.parseInt(qp.get('y0')!, 10) : undefined;
-    const y1 = qp.get('y1') ? Number.parseInt(qp.get('y1')!, 10) : undefined;
+    const x0 = qp.has('x0') ? Number.parseInt(qp.get('x0')!, 10) : 0;
+    const x1 = qp.has('x1') ? Number.parseInt(qp.get('x1')!, 10) : WIDTH - 1;
+    const y0 = qp.get('y0') ? Number.parseInt(qp.get('y0')!, 10) : 0;
+    const y1 = qp.get('y1') ? Number.parseInt(qp.get('y1')!, 10) : HEIGHT - 1;
 
     const rows = selectPixelsInRect.all({ x0, x1, y0, y1 });
     const header = {
