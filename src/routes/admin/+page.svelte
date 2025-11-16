578ba331e38ac801ba6de433bb987bc4940394b0
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-11-16 15:49:25 -0500

    Add admin panel styles and structure to the admin page

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,1 +1,14 @@
-<p>im a admin weeeeeeeeeeeeee</p>
+<svelte:head>
+  <title>Pixel - Admin</title>
+</svelte:head>
+
+<script>
+  import Header from '$lib/components/Header.svelte';
+  import '$lib/assets/styles/admin.css';
+  import '$lib/assets/styles/admin-page.css';
+</script>
+
+<Header message="Admin Panel of Power and Cool"></Header>
+
+<div class="page-content">
+</div>
