d8e88d86f8e904b91a29f6cbe7364ff1aad8cc28
SuperNinjaCat5 <ben.elliott.2021@gmail.com>
2025-11-16 15:15:47 -0500

    Add dynamic admin permission handling and user role checks

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -13,7 +13,7 @@ export const load: PageServerLoad = async ({ locals }) => {
       redirect(302,'/');
     }
     else {
-      if (!user || user.admin_level < 3) {
+      if (!user || user.is_admin < 3) {
         console.log('no access, sending to /home')
         redirect(302,'/home');
       }
